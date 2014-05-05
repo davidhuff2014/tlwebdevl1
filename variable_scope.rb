f0fa5f6508b136ef25595ca829f0cb5f28d245f4
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-05 13:44:33 -0400

    lesson 1 #10 Variable scope

---
+++ variable_scope.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+count = 0
+
+def over_under(count)
+  p count + 1
+  p count - 1
+end
+
+[1, 2, 3].each { |e|  count = e }
+
+def add_one(count)
+  count + 1
+end
+over_under(count)
+p count
+count = add_one(count)
+p count
