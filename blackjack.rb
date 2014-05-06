214c3d0ddeaba24235b26ac3a4ab9f5859634db1
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-05 23:48:48 -0400

    Lesson 1 Blackjack

---
+++ blackjack.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+# cards 4 of each suit or 4 of each
+# 2, 3, 4, 5, 6, 7, 8, 9, 10, J=10, Q=10, K=10, A=11 or 1
+# dealer stays >= 17
+
+puts 'Player, please enter your first name.'
+pname = gets.chomp.to_s
+puts 'Welcome ' + pname + ' to Blackjack where your money will soon be mine'
