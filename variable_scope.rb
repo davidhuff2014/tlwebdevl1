f209993d3705bfbe8732e1c93cb1deabe4715587
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-05 21:51:50 -0400

    learned about mutation and setting

--- variable_scope.rb
+++ variable_scope.rb
@@ -1,18 +1,18 @@
 # encoding: UTF-8
-count = 0
+# count = 0
 
-def over_under(count)
+def over_under(count) # increments does not change
   p count + 1
   p count - 1
 end
 
-[1, 2, 3].each { |e|  count = e }
+[1, 2, 3].each { |e|  count = e } # mutates the count
 
-def add_one(count)
+def add_one(count) # increments but does not change unless set
   count + 1
 end
 over_under(count)
 p count
-count = add_one(count)
-mynum.add_one(count)
+count = add_one(count) # sets the incremented variable
+# p add_one(count) # does not mutate
 p count
