e0a23676062ac93b995e429e499fb2c905349152
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-06 13:02:53 -0400

    bulding deck

--- blackjack.rb
+++ blackjack.rb
@@ -3,6 +3,10 @@
 # 2, 3, 4, 5, 6, 7, 8, 9, 10, J=10, Q=10, K=10, A=11 or 1
 # dealer stays >= 17
 
-puts 'Player, please enter your first name.'
-pname = gets.chomp.to_s
-puts 'Welcome ' + pname + ' to Blackjack where your money will soon be mine'
+# puts 'Player, please enter your first name.'
+# pname = gets.chomp.to_s
+# puts 'Welcome ' + pname + ' to Blackjack where your money will soon be mine'
+c = {  'AC' => 1, '2C' => 2, '3C' => 3, '4C' => 4, '5C' => 5, '6C' => 6,
+       '7C' => 7, '8C' => 8, '9C' => 9, '10C' => 10, 'JC' => 10, 'QC' => 10,
+       'KC' => 10 }
+p c
